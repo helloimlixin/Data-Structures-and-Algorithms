0faf3fb4e1efa326163d25c305c8809354f5d896
Xin Li <bulixin@bu.edu>
2019-09-10 01:43:16 +0800

    Added Manacher Algorithm approach to the Longest Palindromic Substring problem.

--- Dynamic-Programming/src/Problem4_LongestPalindromicSubstring/Main.java
+++ Dynamic-Programming/src/Problem4_LongestPalindromicSubstring/Main.java
@@ -2,24 +2,27 @@ package Problem4_LongestPalindromicSubstring;
 
 public class Main {
     public static void main(String[] args) {
-        String testStr = "asdfghjkllkjhgfdsazxcvbnmmnbvcxzqwertyuioppoiuytrewqqwertyuioplkjhgfdsazxcvbnmmnbvcxzasdfghjklpoiuytrewqasdfghjkllkjhgfdsazxcvbnmmnbvcxzqwertyuioppoiuytrewqqwertyuioplkjhgfdsazxcvbnmmnbvcxzasdfghjklpoiuytrewqasdfghjkllkjhgfdsazxcvbnmmnbvcxzqwertyuioppoiuytrewqqwertyuioplkjhgfdsazxcvbnmmnbvcxzasdfghjklpoiuytrewqasdfghjkllkjhgfdsazxcvbnmmnbvcxzqwertyuioppoiuytrewqqwertyuioplkjhgfdsazxcvbnmmnbvcxzasdfghjklpoiuytrewq";
+        String smallTest = "accca";
+        String testStr = "asdfghjkllkjhgfasdfghjkllkjhgfdsazxcvbnmmnbvcxzqwertyuioppoiuytrewqqwertyuioplkjhgfdsazxcvbnmmnbvcxzasdfghjklpoiuytrewqasdfghjkllkjhgfdsazxcvbnmmnbvcxzqwertyuioppoiuytrewqqwertyuioplkjhgfdsazxcvbnmmnbvcxzasdfghjklpoiuytrewqasdfghjkllkjhgfdsazxcvbnmmnbvcxzqwertyuioppoiuytrewqqwertyuioplkjhgfdsazxcvbnmmnbvcxzasdfghjklpoiuytrewqasdfghjkllkjhgasdfghjkllkjhgfdsazxcvbnmmnbvcxzqwertyuioppoiuytrewqqwertyuioplkjhgfdsazxcvbnmmnbvcxzasdfghjklpoiuytrewqasdfghjkllkjhgfdsazxcvbnmmnbvcxzqwertyuioppoiuytrewqqwertyuioplkjhgfdsazxcvbnmmnbvcxzasdfghjklpoiuytrewqasdfghjkllkjhgfdsazxcvbnmmnbvcxzqwertyuioppoiuytrewqqwertyuioplkjhgfdsazxcvbnmmnbvcxzasdfghjklpoiuytrewqasdfghjkllasdfghjkllkjhgfdsazxcvbnmmnbvcxzqwertyuioppoiuytrewqqwertyuioplkjhgfdsazxcvbnmmnbvcxzasdfghjklpoiuytrewqasdfghjkllkjhgfdsazxcvbnmmnbvcxzqwertyuioppoiuytrewqqwertyuioplkjhgfdsazxcvbnmmnbvcxzasdfghjklpoiuytrewqasdfghjkllkjhgfdsazxcvbnmmnbvcxzqwertyuioppoiuytrewqqwertyuioplkjhgfdsazxcvbnmmnbvcxzasdfghjklpoiuytrasdfghjkllkjhgfdsazxcvbnmmnbvcxzqwertyuioppoiuytrewqqwertyuioplkjhgfdsazxcvbnmmnbvcxzasdfghjklpoiuytrewqasdfghjkllkjhgfdsazxcvbnmmnbvcxzqwertyuioppoiuytrewqqwertyuioplkjhgfdsazxcvbnmmnbvcxzasdfghjklpoiuytrewqasdfghjkllkjhgfdsazxcvbnmmnbvcxzqwertyuioppoiuytrewqqwertyuioplkjhgfdsazxcvbnmmnbvcxzasdfghjklpoiuytrewqasdfghjkllkjhgfdsazxcvbnmmnbvcxzqwertyuioppoiuytrewqqwertyuioplkjhgfdsazxcvbnmmnbvcxzasdfghjklpoiuytrewqewqasdfghjkllkjhgfdsazxcvbnmmnbvcxzqwertyuioppoiuytrewqqwertyuioplkjhgfdsazxcvbnmmnbvcxzasdfghjklpoiuytrewqkjhgfdsazxcvbnmmnbvcxzqwertyuioppoiuytrewqqwertyuioplkjhgfdsazxcvbnmmnbvcxzasdfghjklpoiuytrewqfdsazxcvbnmmnbvcxzqwertyuioppoiuytrewqqwertyuioplkjhgfdsazxcvbnmmnbvcxzasdfghjklpoiuytrewqdsazxcvbnmmnbvcxzqwertyuasdfghjkllkjhgfdsazxcvbnmmnbvcxzqwertyuioppoiuytrewqqwertyuioplkjhgfdsazxcvbnmmnbvcxzasdfghjklpoiuytrewqasdfghjkllkjhgfdsazxcvbnmmnbvcxzqwertyuioppoiuytrewqqwertyuioplkjhgfdsazxcvbnmmnbvcxzasdfghjklpoiuytrewqasdfghjkllkjhgfdsazxcvbnmmnbvcxzqwertyuioppoiuytrewqqwertyuioplkjhgfdsazxcvbnmmnbvcxzasdfghjklpoiuytrewqasdfghjkllkjhgfdsazxcvbnmmnbvcxzqwertyuioppoiuytrewqqwertyuioplkjhgfdsazxcvbnmmnbvcxzasdfghjklpoiuytrewqioppoiuytrewqqwertyuioplkjhgfdsazxcvbnmmnbvcxzasdfghjklpoiuytrewqasdfghjkllkjhgfdsazxcvbnmmnbvcxzqwertyuioppoiuytrewqqwertyuioplkjhgfdsazxcvbnmmnbvcxzasdfghjklpoiuytrewqasdfghjkllkjhgfdsazxcvbnmmnbvcxzqwertyuioppoiuytrewqqwertyuioplkjhgfdsazxcvbnmmnbvcxzasdfghjklpoiuytrewqasdfghjkllkjhgfdsazxcvbnmmnbvcxzqwertyuioppoiuytrewqqwertyuioplkjhgfdsazxcvbnmmnbvcxzasdfghjklpoiuytrewq";
         System.out.println("Dynamic Programming Approach");
         long dpStart = System.nanoTime();
-        System.out.println(DynamicProgamming.longestPalindrome(testStr));
+        String dpResult = DynamicProgamming.longestPalindrome(testStr);
         long dpEnd = System.nanoTime();
         System.out.println("Elapsed time: " + (dpEnd - dpStart));
+        System.out.println("Result: " + dpResult);
         System.out.println("======================");
         System.out.println("Expand Around Center Approach");
         long expandStart = System.nanoTime();
-        System.out.println(ExpandAroundCenter.longestPalindrome(testStr));
+        String expandResult = ExpandAroundCenter.longestPalindrome(testStr);
         long expandEnd = System.nanoTime();
         System.out.println("Elapsed time: " + (expandEnd - expandStart));
+        System.out.println("Result: " + expandResult);
         System.out.println("======================");
         System.out.println("Manacher's Algorithm");
         long manacherStart = System.nanoTime();
-        System.out.println(ManacherAlgorithm.longestPalindrome(testStr));
+        String manacherResult = ManacherAlgorithm.longestPalindrome(testStr);
         long manacherEnd = System.nanoTime();
         System.out.println("Elapsed time: " + (manacherEnd - manacherStart));
-        System.out.println("======================");
+        System.out.println("Result: " + manacherResult);
     }
 }
--- Dynamic-Programming/src/Problem4_LongestPalindromicSubstring/ManacherAlgorithm.java
+++ Dynamic-Programming/src/Problem4_LongestPalindromicSubstring/ManacherAlgorithm.java
@@ -2,6 +2,56 @@ package Problem4_LongestPalindromicSubstring;
 
 class ManacherAlgorithm {
     static String longestPalindrome(String s) {
-        return "";
+        char[] processedStr = preProcessing(s);
+        int N = processedStr.length;
+        // Create an array to store the palindrome numbers.
+        int[] p = new int[N];
+        int center = 0; // mirror line
+        int right = 0; // right bound
+        for (int i = 1; i < N - 1; i++) {
+            // The mirror index on the left of the mirror line.
+            int mirror = center - (i - center);
+            if (right > i) {
+                p[i] = Math.min(right  - i, p[mirror]);
+            }
+            while (processedStr[i + (1 + p[i])] == processedStr[i - (1 + p[i])]) {
+                p[i]++;
+            }
+            if (i + p[i] > right) {
+                center = i;
+                right = i + p[i];
+            }
+        }
+        int maxLen = 0;
+        int centerIdx = 0;
+        for (int i = 1; i < N - 1; i++) {
+            if (p[i] > maxLen) {
+                maxLen = p[i];
+                centerIdx = i;
+            }
+        }
+        return s.substring((centerIdx - 1 - maxLen) / 2, (centerIdx - 1 + maxLen) / 2);
+    }
+
+    /**
+     * Preprocess string str into a char array.
+     * For example, if `s = "abaabac"`, then the resulting char array `chars = "@#a#b#a#a#b#a#c#@"`,
+     * where "#" are interleaved symbol to avoid even/odd-Length palindromes uniformly, and "@" are
+     * prepended and appended to each end to avoid bounds checking.
+     * @param str input string to be processed
+     * @return processed string with "#"'s
+     */
+    private static char[] preProcessing(String str) {
+        int n = str.length();
+        // Here we use char arrays for speed.
+        char[] chars = new char[2 * n + 3];
+        chars[0] = '^';
+        chars[2 * n + 2] = '@';
+        for (int i = 0; i < n; i++) {
+            chars[2 * i + 1] = '#';
+            chars[2 * i + 2] = str.charAt(i);
+        }
+        chars[n * 2 + 1] = '#';
+        return chars;
     }
 }
